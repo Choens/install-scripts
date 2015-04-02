b326395691bd47b12134b2e8ac85c0cde9343c09
Andy Chioens <andy.choens@gmail.com>
2015-04-01 21:40:44 -0400

    Couple of quick files I will use for tracking.
     Changes to be committed:
    	modified:	investigate.txt
    	new file:	ubuntu/experimental.sh

---
+++ ubuntu/experimental.sh
@@ -0,0 +1,8 @@
+## These are things that I have installed, but may not keep.
+
+sudo apt-get install \
+     gpointing-device-settings \
+     udev-discover \
+     uget \
+     fatrat \
+     synaptic \
