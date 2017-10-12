3e04e17fe1f72ff680cdaa844c979170a83fe0ce
Andy Choens <andy.choens@acuitashealth.com>
2017-10-12 16:25:39 -0400

    Removes TBird, install Chromium

--- 03-desktop.sh
+++ 03-desktop.sh
@@ -16,17 +16,11 @@
 # Internet
 # ===============================================================================
 
-# From United RPMs
-# I'm trying to wean myself off Chromium.
-#dnf install chromium
-
-## One are in which Desktop Linux is deficient is email. Evolution, the default
-## GNOME app isn't very good. TBird is better, but has complicated future and is
-## not well integrated with either my desktop OR office suite (libreoffice). I
-## would like to find a better linux desktop email client.
+# Install the Fedora version, not the UnitedRPM version.
 sudo dnf install \
-     thunderbird.x86_64 \
-     thunderbird-enigmail \
-     thunderbird-lightning-gdata.x86_64
+     chromium.x86_64 \
+     fedora-user-agent-chrome \
+     
+
 
 
