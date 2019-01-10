8ff8e7254f5c15f087a14b1bcac3f8354e4da8b6
Andy Choens <andy.choens@acuitashealth.com>
2019-01-10 15:28:01 -0500

    Tweaks to zsh
    Eventually, I need to decide about some zsh stuff . . . .

--- programming/base.sh
+++ programming/base.sh
@@ -16,7 +16,16 @@ sudo dnf install -y \
      rpmdevtools \
      zsh
 
+## This changes the user's default to the zsh shell.
+## Note - the first time you run zsh you will have to answer a bunch of questions.
 chsh -s /usr/bin/zsh
+
+## Loads zcalc
+autoload -Uz zcalc
+
+## Installs Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 
 
 
